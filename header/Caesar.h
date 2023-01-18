40de7f49bf41bf54e1566323a09ceefb369d4087
ArmenParanyan <80101033+ArmParanyan@users.noreply.github.com>
2023-01-18 19:45:03 +0400

    Add files via upload

---
+++ header/Caesar.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+class Caesar
+{
+public:
+    Caesar();
+    ~Caesar();
+
+public:
+    void set_key(int key);
+    int get_key() const;
+    std::string encode(const std::string& text);
+    std::string decode(const std::string& text);
+
+private:
+    int m_key;
+};
